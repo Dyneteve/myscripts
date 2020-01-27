ed585b9a735eec6c5b220a89e0aab07ac82d4302
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-27 21:51:18 +0200

    Meme commit for meme CI

--- kernel.sh
+++ kernel.sh
@@ -44,12 +44,7 @@ case "$ARG1" in
   "violet" ) # Execute violet function / scripts
       export DEVICE="Redmi Note 7 Pro [violet]"
       DEFCONFIG=vendor/violet-perf_defconfig
-      CHATID="-1001245830369"
-  ;;
-  "X00T" ) # Execute X00T function / scripts
-      export DEVICE="ASUS Zenfone Max Pro M1"
-      DEFCONFIG=X00T_defconfig
-      CHATID="-1001181445763"
+      CHATID="-1001408028770"
   ;;
   * ) echo -e "\nError..!! Unknown device. Please add device details to script and re-execute\n"
       return
@@ -109,35 +104,24 @@ fi
 function clone {
 	echo " "
 	echo "★★Cloning GCC Toolchain from Android GoogleSource .."
-	git clone --progress -j32 --depth 5 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9.git
-	git clone --progress -j32 --depth 5 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-
-	#Workaround to remove deprecation spam of gcc
-	cd aarch64-linux-android-4.9
-	git reset --hard 22f053ccdfd0d73aafcceff3419a5fe3c01e878b
-	cd ../arm-linux-androideabi-4.9
-	git reset --hard 42e5864a7d23921858ca8541d52028ff88acb2b6
-	cd $KERNEL_DIR
+	git clone --progress -j32 --depth 1 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -q > /dev/null 2>&1
+	git clone --progress -j32 --depth 1 --no-single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -q > /dev/null 2>&1
 
 	echo "★★GCC cloning done"
 	echo ""
 	echo "★★Cloning Clang 8 sources"
-	wget $CLANG_URL
+	curl -sLo clang-r370808.tar.gz $CLANG_URL
 	mkdir clang-llvm
-	tar -C clang-llvm -xvf clang*.tar.gz
+	tar -C clang-llvm -xvf clang*.tar.gz > /dev/null 2>&1
 	rm -rf clang*.tar.gz
-	echo "★★Clang Done, Now Its time for AnyKernel .."
-	git clone --depth 1 --no-single-branch https://github.com/Panchajanya1999/AnyKernel2.git -b $ARG1
-	echo "★★Cloning libufdt"
-	git clone https://android.googlesource.com/platform/system/libufdt $KERNEL_DIR/scripts/ufdt/libufdt
 	echo "★★Cloning Kinda Done..!!!"
 }
 
 ##------------------------------------------------------##
 
 function exports {
-	export KBUILD_BUILD_USER="panchajanya"
-	export KBUILD_BUILD_HOST="circleci"
+	export KBUILD_BUILD_USER="Dyneteve"
+	export KBUILD_BUILD_HOST="MemeDrone"
 	export ARCH=arm64
 	export SUBARCH=arm64
 	export KBUILD_COMPILER_STRING=$($KERNEL_DIR/clang-llvm/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
@@ -202,33 +186,8 @@ function build_kernel {
 		OBJDUMP=$OBJDUMP \
 		STRIP=$STRIP \
 		CLANG_TRIPLE=aarch64-linux-gnu- 2>&1 | tee error.log
-	make O=out dtbo.img
 	BUILD_END=$(date +"%s")
 	DIFF=$((BUILD_END - BUILD_START))
-	check_img
-}
-
-##-------------------------------------------------------------##
-
-function check_img {
-	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ] 
-	    then
-		gen_zip
-	else
-		tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
-	fi
-}
-
-##--------------------------------------------------------------##
-
-function gen_zip {
-	mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
-	mv $KERNEL_DIR/out/arch/arm64/boot/dtbo.img AnyKernel2/dtbo.img
-	cd AnyKernel2
-	zip -r9 $ZIPNAME-$ARG1-$DATE * -x .git README.md
-	MD5CHECK=$(md5sum $ZIPNAME-$ARG1-$DATE.zip | cut -d' ' -f1)
-	tg_post_build $ZIPNAME* "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
-	cd ..
 }
 
 clone
